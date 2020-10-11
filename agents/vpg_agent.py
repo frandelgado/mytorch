Fix Softmax backward and some other stuff

agents/vpg_agent.py
@@ -10,7 +10,7 @@ from nets.nets import Net
 
 class VPGAgent(Agent):
 
-    def __init__(self, state_space: int, action_space: int, hidden=50, lr=1e-5, gamma=0.9):
+    def __init__(self, state_space: int, action_space: int, hidden=50, lr=1e-2, gamma=0.9):
 
         self.state_space = state_space
         self.action_space = action_space
@@ -20,7 +20,7 @@ class VPGAgent(Agent):
         self.net = Net(
             [
                 {"input_dim": 4, "output_dim": 20, "activation": "sigmoid"},
-                {"input_dim": 20, "output_dim": 2, "activation": "softmax"},
+                {"input_dim": 20, "output_dim": 2, "activation": "sigmoid"},
             ],
             observer=None
         )
@@ -35,7 +35,8 @@ class VPGAgent(Agent):
         state = np.reshape(state, newshape=(self.state_space, -1))
         action_probs, _ = self.net.full_forward_propagation(state)
         action_probs = action_probs.squeeze()
-        action = np.random.choice(2, p=action_probs)
+        action = np.argmax(action_probs)
+        # action = np.random.choice(2, p=action_probs)
         return action, action_probs[action]
 
     def store_transition(self, state, new_state, action, a_prob, reward):
@@ -74,7 +75,6 @@ class VPGAgent(Agent):
             entropy = -np.sum(np.log(probs) * probs)
             actions_batch = actions[batch].numpy()
             probs = np.take_along_axis(probs, actions_batch, axis=0)
-            # 0 bad number
             loss = -(np.log(probs) * rewards[batch])
             dLoss = -1/probs * rewards[batch]
             self.net.train(states_batch, loss, dLoss, epochs=1, learning_rate=self.lr, actions=actions_batch)

envs/cartpole.py
@@ -26,8 +26,8 @@ while True:
 
     for t in range(200):
         prev_obs = observation
-        action, action_prob = agent.act(observation)
-        observation, reward, done, info = env.step(action)
+        action, action_prob = agent.act(prev_obs)
+        observation, reward, done, _ = env.step(action)
         if done:
             break
         agent.store_transition(prev_obs, observation, action, action_prob, reward)
@@ -44,7 +44,8 @@ while True:
         results["entropy"].append((np.mean(entropy_accum)))
         results["episode_length"].append(np.mean(ep_accum))
         results["layers"].append(agent.net.expose_layers())
-        agent.save(i_episode)
+        if i_episode % 50000 == 0:
+            agent.save(i_episode)
         with open("../pickles/results.p", "wb") as file:
             pickle.dump(results, file)
 

nets/activations.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def sigmoid(z):
-    return 1/(1 + np.exp(-z))
+    z = np.clip(z, -500, 500)
+    return 1.0/(1.0 + np.exp(-z))
 
 
 def relu(z):
@@ -59,7 +60,6 @@ def softmax(x):
 def softmax_backward(dA, z, action):
     """
     Calculates dL/dZ
-
     :param dA:
         ndarray representing the vector of partial derivatives dL/dA
     :param z:
@@ -68,13 +68,14 @@ def softmax_backward(dA, z, action):
         ndarray representing the vector of partial derivatives dL/dZ
     """
     z = z.reshape(1, -1).squeeze()
+    S = softmax(z)
     action = action.squeeze()
     dZ = np.zeros(len(z))
     for j in range(len(z)):
         if j == action:
-            dZ[j] = dA * z[action] * (1 - z[action])
+            dZ[j] = dA * S[action] * (1 - S[action])
         else:
-            dZ[j] = - dA * z[action] * z[j]
+            dZ[j] = - dA * S[action] * S[j]
     return dZ.reshape((-1, 1))
 
 

nets/nets.py
@@ -20,10 +20,13 @@ class Net:
             layer_input_size = layer["input_dim"]
             layer_output_size = layer["output_dim"]
 
+            params_values["prevW" + str(layer_idx)] = np.zeros(shape=(layer_output_size, layer_input_size))
+            params_values["prevb" + str(layer_idx)] = np.zeros(shape=(layer_output_size, 1))
+
             params_values['W' + str(layer_idx)] = np.random.randn(
-                layer_output_size, layer_input_size) * 0.1
+                layer_output_size, layer_input_size) * np.sqrt(2/(layer_output_size + layer_input_size))
             params_values['b' + str(layer_idx)] = np.random.randn(
-                layer_output_size, 1) * 0.1
+                layer_output_size, 1) * np.sqrt(2/layer_output_size)
 
         return params_values
 
@@ -119,8 +122,15 @@ class Net:
     def update(self, grads_values, learning_rate):
         for layer_idx, layer in enumerate(self.nn_architecture):
             layer_idx = layer_idx + 1
-            self.params_values["W" + str(layer_idx)] -= learning_rate * grads_values["dW" + str(layer_idx)]
-            self.params_values["b" + str(layer_idx)] -= learning_rate * grads_values["db" + str(layer_idx)]
+            self.params_values["W" + str(layer_idx)] -= learning_rate * (
+                    grads_values["dW" + str(layer_idx)] + 0.8 * self.params_values["prevW" + str(layer_idx)]
+            )
+            self.params_values["b" + str(layer_idx)] -= learning_rate * (
+                    grads_values["db" + str(layer_idx)]  + 0.8 * self.params_values["prevb" + str(layer_idx)]
+            )
+
+            self.params_values["prevW" + str(layer_idx)] = self.params_values["W" + str(layer_idx)]
+            self.params_values["prevb" + str(layer_idx)] = self.params_values["b" + str(layer_idx)]
 
     def get_cost_value(self, Y_hat, Y):
 